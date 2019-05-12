836c46081a04c6e8a5c8da39d9db10a220666776
nick williams <nick5664@outlook.com>
2019-05-12 08:48:00 -0600

    golden tree

--- pureples/es_hyperneat/es_hyperneat_torch.py
+++ pureples/es_hyperneat/es_hyperneat_torch.py
@@ -165,7 +165,7 @@ class ESNetwork:
         for s in range(dimen):
             root_coord.append(0.0)
         #set width and level to 1.0 and 1, assume the substrate been scaled to a unit hypercube
-        root = nDimensionTree(root_coord, 1.0, 1)
+        root = nDimensionGoldenTree(root_coord, 1.0, 1)
         q = [root]
         while q:
             p = q.pop(0)
@@ -445,6 +445,41 @@ class nDimensionTree:
                 new_coord.append(self.coord[y] + (self.width/(2*self.signs[x][y])))
             newby = nDimensionTree(new_coord, self.width/2, self.lvl+1)
             self.cs.append(newby)
+
+class nDimensionGoldenTree:
+
+    def __init__(self, in_coord, width, level):
+        self.w = 0.0
+        self.coord = in_coord
+        self.width = width
+        self.full_width = width * 2
+        self.sub_width = self.full_width / 1.61805
+        self.lvl = level
+        self.num_children = 2**len(self.coord)
+        self.cs = []
+        self.subbed_dimen_count = 0
+        self.lvl = level
+        self.signs = self.set_signs()
+
+    def set_signs(self):
+        return list(itertools.product([1,-1], repeat=len(self.coord)))
+
+    def sub_divide(self):
+        sign = 1
+        golden_cube = []
+        dimen = len(self.coord)
+        #got the appropriate dimensions for out golden cube
+        #we will now permute its position inside the unit hypercube
+        #at the current depth
+        for i in range(self.num_children):
+            child_root = []
+            new_center = 0.0
+            for y in range(dimen):
+                # shift the root inversely from the direction to the center of the sub cube
+                new_center = self.coord[y] + ((self.signs[i][y]*-1)*(self.full_width - self.sub_width))
+                # us new root position in this dimension as spot to offset from
+                child_root.append(new_center + (self.sub_width/(2*self.signs[i][y])))
+            self.cs.append(nDimensionGoldenTree(child_root, self.sub_width/2, self.lvl+1))
     
 # new tree's corresponding connection structure
 class nd_Connection:
