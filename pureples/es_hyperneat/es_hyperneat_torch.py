9c4bedda4e989943a598a5a8bfcf1d06f87ae5c6
nick williams <nick5664@outlook.com>
2019-06-01 08:31:26 -0600

    eh

--- pureples/es_hyperneat/es_hyperneat_torch.py
+++ pureples/es_hyperneat/es_hyperneat_torch.py
@@ -430,6 +430,47 @@ class QuadPoint:
         self.cs = [None] * 4
         self.lvl = lvl
 
+class nDimensionGoldenTree:
+
+    def __init__(self, in_coord, width, level):
+        self.w = 0.0
+        self.coord = in_coord
+        self.width = width
+        self.full_width = width * 2
+        self.sub_width = self.full_width / 1.61805
+        self.offset_dist = self.sub_width - self.width
+        self.lvl = level
+        self.num_children = 2**len(self.coord)
+        self.cs = []
+        self.subbed_dimen_count = 0
+        self.lvl = level
+        self.signs = self.set_signs()
+        self.depth = 0
+
+    def set_signs(self):
+        return list(itertools.product([1,-1], repeat=len(self.coord)))
+
+    def divide_childrens(self):
+        sign = 1
+        golden_cube = []
+        dimen = len(self.coord)
+        #got the appropriate dimensions for out golden cube
+        #we will now permute its position inside the unit hypercube
+        #at the current depth
+        for i in range(self.num_children):
+            child_root = []
+            new_center = 0.0
+            for y in range(dimen):
+                # shift the root inversely from the direction to the center of the sub cube
+                new_center = self.coord[y] + ((self.signs[i][y]*-1)*self.offset_dist)
+                # us new root position in this dimension as spot to offset from
+                child_root.append(new_center + (self.sub_width/(2*self.signs[i][y])))
+            self.cs.append(nDimensionGoldenTree(child_root, self.sub_width/2, self.lvl+1))
+            self.depth += 1
+    
+    @staticmethod
+    def recursive_subdivide(tree, depth):
+        return 
 #
 class nDimensionTree:
 
